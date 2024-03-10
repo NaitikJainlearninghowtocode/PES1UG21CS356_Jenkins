de75124966b5f5a18249c5b2bf64cfe30df7a891
NaitikJainlearninghowtocode <92568170+NaitikJainlearninghowtocode@users.noreply.github.com>
2024-03-10 18:12:48 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World This is naitik jain\n";
+  cout << "Hello, World This is naitik jain and my srn is PES1UG21CS356\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
